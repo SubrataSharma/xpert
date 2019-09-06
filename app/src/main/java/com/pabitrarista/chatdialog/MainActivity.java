201d4e896747e644995d62ac2bd3a37474536f1d
ujjwal <ujjwalmaity98@gmail.com>
2019-09-06 13:11:13 +0530

    Integrate DialogFlow & communicate through voice

--- app/src/main/java/com/pabitrarista/chatdialog/MainActivity.java
+++ app/src/main/java/com/pabitrarista/chatdialog/MainActivity.java
@@ -6,10 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import ai.api.AIListener;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+
+public class MainActivity extends AppCompatActivity implements AIListener {
+
+    EditText editText;
+    TextView textView;
+
+    AIService aiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +42,65 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "You have clicked tittle", Toast.LENGTH_LONG).show();
             }
         });
+
+        init();
+    }
+
+    private void init() {
+        editText = findViewById(R.id.main_editText);
+        textView = findViewById(R.id.main_textView);
+
+        final AIConfiguration config = new AIConfiguration("5a28a24679e6411699636dc44558724c",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
     }
 
     public void funFacts(View view) {
         Intent in = new Intent(MainActivity.this, AskScreen2Activity.class);
         startActivity(in);
         finish();
+    }
+
+    public void sendMsg(View view) {
+        String msg = editText.getText().toString().toString();
+        if (!msg.equals("")) {
+            aiService.startListening();
+//            textView.append(msg + "\n\n");
+        }
+    }
+
+    @Override
+    public void onResult(AIResponse result) {
+        Result result1 = result.getResult();
+//        textView.setText("Query: " + result1.getResolvedQuery() + "\nAction: " + result1.getAction());
+        textView.append(result1.getResolvedQuery() + "\n" + result1.getFulfillment().getSpeech() + "\n\n");
+    }
+
+    @Override
+    public void onError(AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
     }
 }
