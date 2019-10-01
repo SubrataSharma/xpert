a27a5a707076edf8bfd12e696b0502981b827486
ujjwal <ujjwalmaity98@gmail.com>
2019-10-01 15:32:06 +0530

    Set 4th bucket image from 'profession' collection and change 'Welcome Message for fan personas'

--- app/src/main/java/com/pabitrarista/chatdialog/MainActivity.java
+++ app/src/main/java/com/pabitrarista/chatdialog/MainActivity.java
@@ -64,7 +64,8 @@ public class MainActivity extends AppCompatActivity implements AIListener {
     String TAG = "logtag";
 
     CardView cardView_4thBucket;
-    RelativeLayout relativeLayout, relativeLayout_4thBucket;
+    ImageView imageView_4thBucket;
+    RelativeLayout relativeLayout;
     TextView textView, textView_4thBucket;
     HorizontalScrollView horizontalScrollView;
     EditText editText;
@@ -105,6 +106,7 @@ public class MainActivity extends AppCompatActivity implements AIListener {
     private static final String USER_PERSONA_KEY = "user_persona";
     private static final String PROFESION_KEY = "profession";
     private static final String INTEREST_KEY = "interest";
+    private static final String IMAGE_KEY = "image";
     Query content4thBucket, profession;
 
     @Override
@@ -155,7 +157,7 @@ public class MainActivity extends AppCompatActivity implements AIListener {
         cardView_4thBucket = findViewById(R.id.main_4thBucket_cardView);
         cardView_4thBucket.setVisibility(View.GONE);
         relativeLayout = findViewById(R.id.main_relative_layout);
-        relativeLayout_4thBucket = findViewById(R.id.main_4thBucket_relativeLayout);
+        imageView_4thBucket = findViewById(R.id.main_4thBucket_imageView);
         textView = findViewById(R.id.main_text_view);
         textView_4thBucket = findViewById(R.id.main_4thBucket_textView);
         horizontalScrollView = findViewById(R.id.main_horizontal_scroll_view);
@@ -317,12 +319,8 @@ public class MainActivity extends AppCompatActivity implements AIListener {
                 msg = new ChatViewData(ChatViewData.MSG_TYPE_RECEIVED, s2);
                 recyclerView.smoothScrollToPosition(chatViewAdapter.addChatData(msg));
 
-                String s3 = "Thank you for your support.";
+                String s3 = "Thank you for your support,\nHow are you doing today?";
                 msg = new ChatViewData(ChatViewData.MSG_TYPE_RECEIVED, s3);
-                recyclerView.smoothScrollToPosition(chatViewAdapter.addChatData(msg));
-
-                String s4 = "How are you doing today?";
-                msg = new ChatViewData(ChatViewData.MSG_TYPE_RECEIVED, s4);
                 recyclerView.smoothScrollToPosition(chatViewAdapter.addChatData(msg));
             }
         });
@@ -388,15 +386,17 @@ public class MainActivity extends AppCompatActivity implements AIListener {
                                             @Override
                                             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                                                 if (task.isSuccessful()) {
-                                                    String prof, inters;
+                                                    String prof, inters, imageUrl;
                                                     try {
                                                         for (QueryDocumentSnapshot document : task.getResult()) {
                                                             prof = document.getString(PROFESSION_KEY);
                                                             inters = document.getString(INTEREST_KEY);
+                                                            imageUrl = document.getString(IMAGE_KEY);
                                                             inters = inters.replace('-', ' ');
                                                             inters = inters.toUpperCase();
                                                             textView_4thBucket.setText(inters);
-                                                            set4thBucketImage(inters);
+                                                            Picasso.get().load(imageUrl).into(imageView_4thBucket);
+                                                            cardView_4thBucket.setVisibility(View.VISIBLE);
                                                         }
                                                     } catch (Exception e) {
                                                         e.printStackTrace();
@@ -410,81 +410,6 @@ public class MainActivity extends AppCompatActivity implements AIListener {
                         }
                     }
                 });
-    }
-
-    private void set4thBucketImage(String str) {
-        switch (str) {
-            case "ACTOR":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_movies);
-                break;
-            case "BADMINTON PLAYER":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_badminton);
-                break;
-            case "BOOK WRITER":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_writers);
-                break;
-            case "CHEF":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_food);
-                break;
-            case "CHOREOGRAPHER":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_choreographers);
-                break;
-            case "COMEDIAN":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_comedy);
-                break;
-            case "CRICKETER":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_cricket);
-                break;
-            case "DIRECTOR":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_movies);
-                break;
-            case "ENTREPRENEUR":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_business);
-                break;
-            case "FASHION DESIGNER":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_fashion_design);
-                break;
-            case "FOOTBALLER":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_football);
-                break;
-            case "INDUSTRIALIST":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_business);
-                break;
-            case "INVESTOR":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_business);
-                break;
-            case "MAKEUP ARTIST":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_beauty);
-                break;
-            case "MUSIC COMPOSER":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_music_composers);
-                break;
-            case "NUTRITIONIST":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_nutrition);
-                break;
-            case "PAINTER":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_paint);
-                break;
-            case "PHOTOGRAPHER":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_photography);
-                break;
-            case "POLITICIAN":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_politics);
-                break;
-            case "SINGER":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_singing);
-                break;
-            case "SPIRITUAL GURU":
-//                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_singing);
-                break;
-            case "TENNIS PLAYER":
-                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_tennis);
-                break;
-            case "VENTURE CAPITALIST":
-//                relativeLayout_4thBucket.setBackgroundResource(R.drawable.image_bucket_singing);
-                break;
-        }
-        cardView_4thBucket.setVisibility(View.VISIBLE);
     }
 
     private void hideBucket() {
